ee8ec2a1d60e596c1798107565fac78f965a9390
Nicole L <nicole.chunyang.li@gmail.com>
2025-03-10 14:49:37 +1100

    revert changes in appConfig

--- appConfig.js
+++ appConfig.js
@@ -11,9 +11,9 @@ module.exports = {
     port: 3306,
     user: "root",
     password: "12345678",
+    database: "moocdb",
   },
   redisConfig: {
-    database: "moocdb",
     host: "127.0.0.1",
     port: 6379,
   },
