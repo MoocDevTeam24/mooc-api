92d33ea423ab379c7e589ea88436652781a6e98f
Nicole L <nicole.chunyang.li@gmail.com>
2025-03-11 23:53:31 +1100

    add category file in migrations, models and seeders

--- models/category.js
+++ models/category.js
@@ -1,47 +1,69 @@
-const { DataTypes } = require("sequelize");
-const { sequelize } = require("../db/sequelizedb");
-
-const categoryModel = sequelize.define(
-  "categoryModel",
-  {
-    // Model attributes are defined here
-    ID: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Category.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      CategoryName: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      Description: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      ParentId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      Created_At: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+      },
+      Updated_At: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+      },
+      Created_By: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: user,
+          key: "id",
+        },
+      },
+      Updated_By: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: user,
+          key: "id",
+        },
+      },
+      IconUrl: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
     },
-    CategoryName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    Description: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    ParentId: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    Created_At: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      allowNull: false,
-    },
-    Updated_At: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-      allowNull: false,
-    },
-    // Created_By: {
-    //   type: DataTypes.INTEGER,
-    // },
-    IconUrl: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-  },
-  { timestamps: false, tableName: "Category" }
-);
-
-module.exports = categoryModel;
+    {
+      sequelize,
+      modelName: "Category",
+    }
+  );
+  return Category;
+};
