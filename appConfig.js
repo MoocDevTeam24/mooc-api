40fa14ccb54d8370f4ca028500b851f5ebe9e848
sharon <xuanrong.sharon@gmail.com>
2025-03-12 19:08:40 +0800

    minor fix

--- appConfig.js
+++ appConfig.js
@@ -10,7 +10,7 @@ module.exports = {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "12345678",
+    password: "123456789",
     database: "moocdb",
   },
   redisConfig: {
