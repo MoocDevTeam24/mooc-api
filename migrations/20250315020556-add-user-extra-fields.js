719fe2f9fdd0cbe1540a375c93bc3cebce161136
Ray Runze Wang <rayrunzewang@gmail.com>
2025-03-15 21:54:28 +0800

    fix(user): add user extra fields

--- migrations/20250315020556-add-user-extra-fields.js
+++ migrations/20250315020556-add-user-extra-fields.js
@@ -1,50 +1,29 @@
 "use strict";
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("demo", {
-      // Model attributes are defined here
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      title: {
-        type: Sequelize.STRING(200),
-        allowNull: false,
-      },
-      mark: {
-        type: Sequelize.STRING(500),
-        allowNull: true,
-      },
-      count: {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-      },
-      acitve: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: true,
-        allowNull: false,
-      },
-      dataTime: {
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW,
-        allowNull: false,
-      },
+    await queryInterface.addColumn("user", "avatar", {
+      type: Sequelize.STRING(255),
+      allowNull: true,
     });
-    console.log("table demo created");
+    await queryInterface.addColumn("user", "nickname", {
+      type: Sequelize.STRING(255),
+      allowNull: true,
+    });
+    await queryInterface.addColumn("user", "active", {
+      type: Sequelize.BOOLEAN,
+      defaultValue: true,
+    });
+    await queryInterface.addColumn("user", "access", {
+      type: Sequelize.STRING(255),
+      allowNull: true,
+    });
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-    await queryInterface.dropTable("demo");
-    console.log("table demo droped");
+    await queryInterface.removeColumn("user", "avatar");
+    await queryInterface.removeColumn("user", "nickname");
+    await queryInterface.removeColumn("user", "active");
+    await queryInterface.removeColumn("user", "access");
   },
 };
